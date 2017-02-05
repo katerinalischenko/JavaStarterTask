0640bb2c584b44328023de27490baf939bacb4ad
katerinalischenko <katerina.lischenko@gmail.com>
2017-02-05 17:51:40 +0200

    Update TaskCycle1.java

--- TaskCycle1.java
+++ TaskCycle1.java
@@ -4,8 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
+//С помощью циклов нарисовать «обои». Причем количество полос
+//должно вводиться с клавиатуры. В примере 7 полос.
+//***+++***+++***+++***
+//***+++***+++***+++***
+//***+++***+++***+++***
+//***+++***+++***+++***
+//***+++***+++***+++***
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number");
 		int number = sc.nextInt();
