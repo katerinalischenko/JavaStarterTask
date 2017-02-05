6311ce9cfb1b21b9822da34f54d35e55a65793d3
katerinalischenko <katerina.lischenko@gmail.com>
2017-02-05 17:52:30 +0200

    Update TaskCycle2.java

--- TaskCycle2.java
+++ TaskCycle2.java
@@ -3,9 +3,13 @@ package com.gmail.katryskin;
 import java.util.Scanner;
 
 public class Main {
+	
+	//Вычислить с помощью цикла факториал числа - n введенного с
+	//клавиатуры (4<n<16). Факториал числа это произведение всех чисел от
+	//этого числа до 1. Например 5!=5*4*3*2*1=120
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 	int n;
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Input n");
